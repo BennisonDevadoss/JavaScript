1a3842e1ac8da1268468c849910f8b450f3ef088
Bennison Devadoss <bennisondevadoss@gmail.com>
2022-01-27 21:51:07 +0530

    Date: 27: 01: 2022

---
+++ arity.js
@@ -0,0 +1,16 @@
+// This program is about to what Arity is 
+
+function fun(a, b, c){
+	console.log("Hello World!"); 
+
+}
+
+let arity = fun.length; 
+
+console.log(arity); 
+
+// This program will give the output Three(3)
+//
+//Beacause the above program take the three parameter 
+//this program counts the parameters of function. 
+
