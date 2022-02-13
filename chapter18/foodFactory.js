06e43478a4fc37630638a21a41e942539c029df4
Bennison Devadoss <bennisondevadoss@gmail.com>
2022-02-13 17:56:19 +0530

    Date: 13: 02: 2022

---
+++ chapter18/foodFactory.js
@@ -0,0 +1,16 @@
+const { print } = require("./print")
+const { Ingredient } = require("./ingredient")
+
+class FoodFactory {
+    // empty constructor;
+    // we will not be instantiating any objects here. 
+    constructor() {
+
+    }
+};
+
+FoodFactory.make = function (what) {
+    return new Ingredient(what.name, what.type, what.calories)
+};
+
+modules.exports = { FoodFactory }; 
---
+++ chapter18/print.js
@@ -0,0 +1,5 @@
+// Overwrite console.log to print
+
+const print = (message) => console.log(message);
+
+module.exports = { print }; 
