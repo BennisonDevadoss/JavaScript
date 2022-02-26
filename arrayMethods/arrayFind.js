c5d70c8b16a4e61a38d215d8cff34369f9d1d79b
Bennison Devadoss <bennisondevadoss@gmail.com>
2022-02-26 16:50:20 +0530

    Date: 26: 02: 2022

--- arrayMethods/arrayFind.js
+++ arrayMethods/arrayFind.js
@@ -1,6 +1,7 @@
 /* The find() method returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned. */
 /* REFERENCE
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find */
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find 
+*/
 const array = [12, 12, 14, 14, 12, 15, 26, 25];
 
 const found = array.find(element => {
